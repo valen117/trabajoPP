5533f3defc6bd6c40b59507f0078d242d3e7cdd4
valen117 <valenbello1234@gmail.com>
2023-06-30 11:06:38 -0300

    haciendo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ItemsComponent } from './components/items/items.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ItemsComponent
+    ItemsComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
-export class ItemsComponent {
+export class ItemsComponent{
 
+items:Item[]=[];
+
+ngOnInit():void{
+  this.items=[
+    {
+      id: 0,
+      title:'manzana',
+      price:10.5,
+      quantity:4,
+      completed:false,
+
+    },
+    {
+      id: 0,
+      title:'papa',
+      price:7,
+      quantity:3,
+      completed:true,
+
+    },
+  ]
+}
 }
